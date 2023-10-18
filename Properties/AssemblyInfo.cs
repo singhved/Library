2e1415790ae058ed024fcddf7e867652a46e865b
singhved <vedprakashs039@gmail.com>
2023-10-18 12:03:43 +0530

    push of the day 18-10-2023

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("06b9be1e-a56c-4a1b-b101-f2ef9491cd84")]
 
-// Version information for an assembly consists of the following four values:
+// Version information for an assembly consists of the following four values:;
 //
 //      Major Version
 //      Minor Version
